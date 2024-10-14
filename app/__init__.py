e16f59b0edeaa00b0d741554ed1fdc11fc69bc1d
progchris00 <prog.christian07@gmail.com>
2024-10-14 17:28:24 +0800

    feat: add images

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, '.app.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
 
     if test_config is None:
@@ -19,3 +19,11 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
---
+++ app/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE book (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  category TEXT NOT NULL,
+  is_borrowed INTEGER NOT NULL,
+  borrower_id INTEGER,
+  FOREIGN KEY (borrower_id) REFERENCES user (id)
+);
