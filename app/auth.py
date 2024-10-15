3e7c2e30e70eee17ec5edf8d66a4d4759f197792
progchris00 <prog.christian07@gmail.com>
2024-10-15 09:04:07 +0800

    feat: redirect to the correct view

--- app/auth.py
+++ app/auth.py
@@ -57,7 +57,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('book.view'))
+            return redirect(url_for('book.index'))
         
         flash(error)
     
