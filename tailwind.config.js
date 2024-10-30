acf2824773a185eb64c08a58325715de9238be21
progchris00 <prog.christian07@gmail.com>
2024-10-30 08:22:40 +0800

    feat: extend color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./app/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "site-bg": "#050d1a",
+        table: "#0d111d",
+        thead: "#171b26",
+        "success-600": "#74dfa2",
+        success: "#133a30",
+        "warning-600": "#f31260",
+        warning: "#3f1630",
+        "custom-white": "#f1f9f8",
+      },
+      boxShadow: {
+        custom:
+          "0px 0px 5px 0px rgba(0,0,0,.05),0px 2px 10px 0px rgba(0,0,0,.2),inset 0px 0px 1px 0px hsla(0,0%,100%,.15)",
+      },
+    },
   },
   plugins: [],
 };
