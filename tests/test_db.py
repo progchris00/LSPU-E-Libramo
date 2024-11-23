81461418285ebdb158276e4ffdc58e5dfa55bdff
progchris00 <prog.christian07@gmail.com>
2024-11-23 17:38:37 +0800

    feat: add init to test db

--- tests/test_db.py
+++ tests/test_db.py
@@ -13,3 +13,15 @@ def test_get_close_db(app):
         db.execute('SELECT 1')
 
     assert 'closed' in str(e.value)
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('app.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
